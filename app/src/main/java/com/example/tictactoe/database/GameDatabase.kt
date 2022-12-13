d5e45e62d78d4ee8bc36d8fdda3092ba5e6a2e9d
ofekblander <ofek.blander@is.com>
2022-12-13 16:51:49 +0200

    presenter and view

--- app/src/main/java/com/example/tictactoe/database/GameDatabase.kt
+++ app/src/main/java/com/example/tictactoe/database/GameDatabase.kt
@@ -36,6 +36,7 @@ abstract class GameDatabase : RoomDatabase() {
         }
     }
 
+
     private class GameDatabaseCallback(private val scope: CoroutineScope) : RoomDatabase.Callback(){
         override fun onCreate(db: SupportSQLiteDatabase) {
             super.onCreate(db)
